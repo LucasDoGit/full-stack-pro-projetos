6b0b37e36556726b08e395cd568ee55e3d3c474a
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-04-24 23:10:11 -0300

    tipagem dinamica

---
+++ typescrit/projeto2/build/type_inference.js
@@ -0,0 +1,12 @@
+"use strict";
+// Tipagem DINAMICA
+let curso = "TypeScript";
+// curso = 150; // typescript nao aceita mudar o tipo de uma variavel apos a declaracao
+let tecnologias = [
+    "PHP",
+    "React JS",
+    // 2021 se desclarar numeros no array ele passa a aceitar
+];
+tecnologias.push("Reac Native");
+// tecnologias.push(2024); // o mesmo vale para array
+console.log(tecnologias);
