59817e90b3d218235064615cc97426b098cd669b
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-05-10 17:30:31 -0300

    useStates com Typescript

--- react-typescript/primeiro/src/App.tsx
+++ react-typescript/primeiro/src/App.tsx
@@ -1,13 +1,73 @@
-import { Header } from "./components/header"
-import { Aluno } from "./components/aluno"
-import { Footer } from "./components/footer"
+import { useState } from "react"
+
+interface InfoAlunoProps {
+  nome: string,
+  idade: string
+}
 
 export default function App(){
+  const [nome, setNome] = useState("")
+  const [idade, setIdade] = useState("")
+
+  const [infoAluno, setInfoAluno] = useState<InfoAlunoProps>()
+
+  const [contador, setContador] = useState<number>(0)
+
+
+  function mostrarAluno(){
+    
+    setInfoAluno({
+      nome: nome,
+      idade: idade
+    })
+
+  }
+
+  function adicionar(){
+    setContador(valorAtual => valorAtual +1)
+  }
+
+  function diminuir(){
+    if(contador===0) return
+    setContador(valorAtual => valorAtual -1)
+  }
+
   return(
     <div>
-      <Header title="Estudando TSX" />
-      <Aluno nome="Lucas Timoteo" idade={21} />
-      <Footer/>
+      <h1>Conhecendo useState</h1>
+
+      <input 
+        placeholder="Digite o seu nome"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+
+      <br/>
+      <br/>
+
+      <input 
+        placeholder="Digite a sua idade"
+        value={idade}
+        onChange={(e) => setIdade(e.target.value)}
+      />
+
+      <br/>
+      <br/>
+      
+
+      <button onClick={mostrarAluno}>Mostrar Aluno</button>
+
+      <hr/>
+
+      <h3>Bem vindo: {infoAluno?.nome}</h3>
+      <h4>Idade: {infoAluno?.idade}</h4>
+
+      <hr/>
+      <br/>
+
+      <h1>Contador com useState</h1>
+
+      <button onClick={adicionar}>+</button> {contador} <button onClick={diminuir}>-</button>
     </div>
   )
 }
--- react-typescript/primeiro/src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer(){
-    return(
-        <footer>
-            Todos os Direitos reservados- @LucasTimoteo
-        </footer>
-    )
-}
