394805b3082260b272ac1177283248234bff6dda
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2025-01-09 17:24:17 -0300

    feat: criando middlewares para rotas

--- node/primeira-api/src/routes.ts
+++ node/primeira-api/src/routes.ts
@@ -1,10 +1,73 @@
-import { Router, Response, Request } from "express";
+import { Router, Response, Request, NextFunction } from "express";
 
 const router = Router();
 
-router.get("/tarefas", (req: Request, res: Response) => {
+const tarefas =  ["Estudar NodeJS", "Estudar javascript"]
+
+router.use((req: Request, res: Response, next: NextFunction) => {
+    console.log("Passou por um middleware")
+
+    return next();
+})
+
+function checkTarefa(req: Request, res: Response, next: NextFunction){
+    if(!req.body.name){
+        res.status(400).json({ message: "Nome invalido" })
+        return;
+    }
+
+    return next();
+}
+
+function checkIndexTarefa(req: Request, res: Response, next: NextFunction){
     
-    res.json({ message: "Minha primeira API", aluno: "Lucas Timoteo" })
+    const tarefa = tarefas[Number(req.params.index)]
+
+    if(!tarefa){
+        res.status(400).json({ error: "Tarefa não encontrada!"})
+        return;
+    }
+
+    return next();
+}
+
+
+router.get("/tarefas", (req: Request, res: Response) => {
+
+    res.json(tarefas)
+    
+})
+
+router.get("/tarefas/:index", checkIndexTarefa, (req: Request, res: Response) => {
+    const index = req.params.index;
+    
+    res.json({ tarefa: tarefas[Number(index)] })
+})
+
+router.post("/tarefas", checkTarefa, (req: Request, res: Response) => {
+    const { name } = req.body;
+
+    tarefas.push(name)
+    
+    res.json(tarefas)
+})
+
+router.put("/tarefas/:index", checkTarefa, checkIndexTarefa, (req: Request, res: Response) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    tarefas[Number(index)] = name;
+
+    res.json(tarefas)
+
+})
+
+router.delete("/tarefas/:index", checkIndexTarefa, (req: Request, res: Response) => {
+    const { index } = req.params;
+
+    tarefas.splice(Number(index));
+
+    res.json({ message: "Task deleted"})
 })
 
 export { router }
